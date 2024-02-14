06e5383c1cc45bd607d3d0f3250ea988db1f0f86
prathiksha-naik <prathiksha07416@gmail.com>
2024-02-14 17:54:22 +0530

    Revert "Revert "Revert "commit"""
    This reverts commit 0acdfeae7abac293c783977821c43838849e8546.

--- ClothingStore.Application/DataTransferObjects/ClothItemDto.cs
+++ ClothingStore.Application/DataTransferObjects/ClothItemDto.cs
@@ -16,5 +16,6 @@
 
         public string? Description { get; set; }
 
+
     }
 }
---
+++ ClothingStore.Application/DataTransferObjects/UserLoginDto.cs
@@ -0,0 +1,8 @@
+﻿namespace ClothStoreApplication.DataTransferObjects
+{
+    public class UserLoginDto
+    {
+        public string Username { get; set; } = null!;
+        public string Password { get; set; } = null!;
+    }
+}
