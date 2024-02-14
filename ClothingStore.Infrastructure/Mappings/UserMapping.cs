4626dfefdfe19e311ef488d432c5752f99adfc93
prathiksha-naik <prathiksha07416@gmail.com>
2024-02-14 15:01:19 +0530

    Revert "Merge branch 'main' of https://github.com/prathiksha-naik/cloth-store-app"
    This reverts commit 637b7f2faf14443de9da48b69a863b25b5f57c97, reversing
    changes made to d98ff0383ac8e51a69bc272d1f0b7f31267bb105.

---
+++ ClothingStore.Infrastructure/Mappings/UserMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ClothingStore.Domain.Entities;
+using ClothStoreApplication.DataTransferObjects;
+
+namespace ClothStoreInfrastructure.Mappings
+{
+    public class UserMapping : Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<User, UserRegisterDto>();
+            CreateMap<UserRegisterDto, User>();
+        }
+    }
+}
